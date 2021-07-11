4342e26c8432ad12ea68e1f10281c03d96d4e689
Milan Vidakovic <mvidakovic@gmail.com>
2021-07-11 18:31:00 +0200

    cleanup

--- asmb.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM Usage: asm.bat program.asm
-if %1asdf==asdf goto usage
-customasm.exe -i instruction-set.txt -o %1.bin -f binary %1 -s %1.sym
-goto end
-:usage
-ECHO Usage: asm.bat program.asm
-:end
